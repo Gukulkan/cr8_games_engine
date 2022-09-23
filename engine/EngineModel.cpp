bec00af4002deebf005c550f0de0dda09346ba69
gukulkan <gukulkan@gmail.com>
2022-09-23 12:39:42 -0500

    Add file loader

--- engine/EngineModel.cpp
+++ engine/EngineModel.cpp
@@ -1,8 +1,28 @@
 #include "EngineModel.h"
 
+#include "EngineUtils.h"
+
+// libs
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <vendor/tinyobjectloader/tiny_obj_loader.h>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+
 // std
 #include <cassert>
 #include <cstring>
+#include <unordered_map>
+
+namespace std {
+    template <>
+    struct hash<EngineModel::Vertex> {
+        size_t operator()(EngineModel::Vertex const &vertex) const {
+            size_t seed = 0;
+            engine::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+            return seed;
+        }
+    };
+}
 
 EngineModel::EngineModel(EngineDevice &device, const EngineModel::Builder &builder) : engineDevice{device} {
     createVertexBuffers(builder.vertices);
@@ -101,6 +121,12 @@ void EngineModel::bind(VkCommandBuffer commandBuffer) {
     }
 }
 
+std::unique_ptr<EngineModel> EngineModel::createModelFromFile(EngineDevice &device, const std::string &filepath) {
+    Builder builder{};
+    builder.loadModel(filepath);
+    return std::make_unique<EngineModel>(device, builder);
+}
+
 std::vector<VkVertexInputBindingDescription> EngineModel::Vertex::getBindingDescriptions() {
     std::vector<VkVertexInputBindingDescription> bindingDescriptions(1);
     bindingDescriptions[0].binding = 0;
@@ -122,3 +148,65 @@ std::vector<VkVertexInputAttributeDescription> EngineModel::Vertex::getAttribute
     attributeDescriptions[1].offset = offsetof(Vertex, color);
     return attributeDescriptions;
 }
+
+void EngineModel::Builder::loadModel(const std::string &filepath) {
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+    std::string warn, err;
+
+    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str())) {
+        throw std::runtime_error(warn + err);
+    }
+
+    vertices.clear();
+    indices.clear();
+
+    std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+    for (const auto &shape : shapes) {
+        for (const auto &index : shape.mesh.indices) {
+            Vertex vertex{};
+
+            if (index.vertex_index >= 0) {
+                vertex.position = {
+                        attrib.vertices[3 * index.vertex_index + 0],
+                        attrib.vertices[3 * index.vertex_index + 1],
+                        attrib.vertices[3 * index.vertex_index + 2],
+                };
+
+                auto colorIndex = 3 * index.vertex_index + 2;
+                if (colorIndex < attrib.colors.size()) {
+                    vertex.color = {
+                            attrib.colors[colorIndex - 2],
+                            attrib.colors[colorIndex - 1],
+                            attrib.colors[colorIndex - 0],
+                    };
+                } else {
+                    vertex.color = {1.f, 1.f, 1.f};  // set default color
+                }
+            }
+
+            if (index.normal_index >= 0) {
+                vertex.normal = {
+                        attrib.normals[3 * index.normal_index + 0],
+                        attrib.normals[3 * index.normal_index + 1],
+                        attrib.normals[3 * index.normal_index + 2],
+                };
+            }
+
+            if (index.texcoord_index >= 0) {
+                vertex.uv = {
+                        attrib.texcoords[2 * index.texcoord_index + 0],
+                        attrib.texcoords[2 * index.texcoord_index + 1],
+                };
+            }
+
+            if (uniqueVertices.count(vertex) == 0) {
+                uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+                vertices.push_back(vertex);
+            }
+            indices.push_back(uniqueVertices[vertex]);
+        }
+    }
+}
+
--- sandbox/SandboxApp.cpp
+++ sandbox/SandboxApp.cpp
@@ -56,62 +56,11 @@ void SandboxApp::run() {
     vkDeviceWaitIdle(engineDevice.device());
 }
 
-std::unique_ptr<EngineModel> createCubeModel(EngineDevice &device, glm::vec3 offset) {
-    EngineModel::Builder modelBuilder{};
-
-    modelBuilder.vertices = {
-
-            // left face (white)
-            {{-.5f, -.5f, -.5f},  {.9f, .9f, .9f}},
-            {{-.5f, .5f,  .5f},   {.9f, .9f, .9f}},
-            {{-.5f, -.5f, .5f},   {.9f, .9f, .9f}},
-            {{-.5f, .5f,  -.5f},  {.9f, .9f, .9f}},
-
-            // right face (yellow)
-            {{.5f,  -.5f, -.5f},  {.8f, .8f, .1f}},
-            {{.5f,  .5f,  .5f},   {.8f, .8f, .1f}},
-            {{.5f,  -.5f, .5f},   {.8f, .8f, .1f}},
-            {{.5f,  .5f,  -.5f},  {.8f, .8f, .1f}},
-
-            // top face (orange, remember y axis points down)
-            {{-.5f, -.5f, -.5f},  {.9f, .6f, .1f}},
-            {{.5f,  -.5f, .5f},   {.9f, .6f, .1f}},
-            {{-.5f, -.5f, .5f},   {.9f, .6f, .1f}},
-            {{.5f,  -.5f, -.5f},  {.9f, .6f, .1f}},
-
-            // bottom face (red)
-            {{-.5f, .5f,  -.5f},  {.8f, .1f, .1f}},
-            {{.5f,  .5f,  .5f},   {.8f, .1f, .1f}},
-            {{-.5f, .5f,  .5f},   {.8f, .1f, .1f}},
-            {{.5f,  .5f,  -.5f},  {.8f, .1f, .1f}},
-
-            // nose face (blue)
-            {{-.5f, -.5f, 0.5f},  {.1f, .1f, .8f}},
-            {{.5f,  .5f,  0.5f},  {.1f, .1f, .8f}},
-            {{-.5f, .5f,  0.5f},  {.1f, .1f, .8f}},
-            {{.5f,  -.5f, 0.5f},  {.1f, .1f, .8f}},
-
-            // tail face (green)
-            {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-            {{.5f,  .5f,  -0.5f}, {.1f, .8f, .1f}},
-            {{-.5f, .5f,  -0.5f}, {.1f, .8f, .1f}},
-            {{.5f,  -.5f, -0.5f}, {.1f, .8f, .1f}},
-
-    };
-    for (auto& v : modelBuilder.vertices) {
-        v.position += offset;
-    }
-    modelBuilder.indices = {0,  1,  2,  0,  3,  1,  4,  5,  6,  4,  7,  5,  8,  9,  10, 8,  11, 9,
-                            12, 13, 14, 12, 15, 13, 16, 17, 18, 16, 19, 17, 20, 21, 22, 20, 23, 21};
-
-    return std::make_unique<EngineModel>(device, modelBuilder);
-}
-
 void SandboxApp::loadGameObjects() {
-    std::shared_ptr<EngineModel> engineModel = createCubeModel(engineDevice, {.0f, .0f, .0f});
-    auto cube = EngineGameObject::createGameObject();
-    cube.model = engineModel;
-    cube.transform.translation = {.0f, .0f, 2.5f};
-    cube.transform.scale = {.5f, .5f, .5f};
-    gameObjects.push_back(std::move(cube));
+    std::shared_ptr<EngineModel> engineModel = EngineModel::createModelFromFile(engineDevice, "models/smooth_vase.obj");
+    auto gameObj = EngineGameObject::createGameObject();
+    gameObj.model = engineModel;
+    gameObj.transform.translation = {.0f, .0f, 2.5f};
+    gameObj.transform.scale = glm::vec3(3.f);
+    gameObjects.push_back(std::move(gameObj));
 }
